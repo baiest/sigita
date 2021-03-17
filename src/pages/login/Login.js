70f4744e530dfb5a3c9376ad00f9f6c789c05989
baiest <ballesteros.juan@correounivalle.edu.co>
2021-03-17 17:08:17 -0500

    Organizacion de paginas agregada

---
+++ src/pages/login/Login.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Pagina de login</h1>
+            </div>
+            );
+    }
+}
