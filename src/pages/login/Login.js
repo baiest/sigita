71580ddd46296ed7c788088c80a26e88c28df686
baiest <ballesteros.juan@correounivalle.edu.co>
2021-03-18 22:24:41 -0500

    Login con google agregado

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,17 @@
 import { Button, Form } from "react-bootstrap";
 import { Component } from "react";
+import { GoogleLogin } from "react-google-login";
 import './Login.css';
 
+const clientId = '4gd9i3tvp8jl0p7h1sm0avejj135ljoj.apps.googleusercontent.com'
+
+const onSuccess = (res) =>{
+    console.log('[Login success] currentUser:', res.profileOnj);
+}
+
+const onFailure = (res) => {
+    console.log('[Login failed] res:', res);
+}
 class Login extends Component {
     render() {
         return (
@@ -17,8 +27,18 @@ class Login extends Component {
                 <Form.Group>
                     <Button type="submit" variant="outline-warning" size="lg" block>Entrar</Button>
                 </Form.Group>
+                
+                <Form.Group>
+                    <GoogleLogin
+                        clientId={clientId}
+                        buttonText="Entra con Google"
+                        onSuccess={onSuccess}
+                        onFailure={onFailure}                    
+                    />
+                </Form.Group>
             </Form>
-            
+
+
             <a href="#CorreoContraseña">¿Olvidaste tu contraseña?</a>
             <a href="#Terminos"><small>Terminos y condiciones</small></a>
         </div>
